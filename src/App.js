db9e6b912afa26433a5074f2ab271582873d6c6b
vov62 <avivovgen1@gmail.com>
2023-04-13 14:52:33 +0300

    responsive mobile fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Layout from "./Pages/Layout/Layout";
 
 function App() {
   return (
-    <div className="App" style={{ overflow: "hidden" }}>
+    <div className="App">
       <Layout />
     </div>
   );
